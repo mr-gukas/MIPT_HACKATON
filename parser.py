ed31579545f3ea3cc2f5a2d9d92a1e7694978a5f
ILUHAAAA <pramskii.is@phystech.edu>
2024-10-13 14:52:58 +0300

    Updated version of parses, using lib mistune to handle more different types of special md-file commands

--- parser.py
+++ parser.py
@@ -1,19 +1,16 @@
 import os
-import markdown
 from collections import Counter
 from bs4 import BeautifulSoup
 import json
 import mistune
 
-
 class File_parser():
+    __slots__ = ('md_files', 'parsed_res')
     def __init__(self, path='.', ext='.md'):
         self.md_files = [f for f in os.listdir(path) if f.endswith(ext)]
         if not self.md_files:
             print('Тут надо сделать обработку запуска, если нет мд файлов')
             exit(0)
-
-        self.markdown_file = markdown.Markdown(extensions=['toc'])
         
         self.parsed_res = [0] * len(self.md_files)
 
@@ -21,11 +18,9 @@ class File_parser():
         for file_num, file_name in enumerate(self.md_files):
             with open(file_name, 'r'  ,encoding='utf-8') as f:
                 parse = BeautifulSoup(mistune.html(f.read()), features='lxml')
-                # parse = BeautifulSoup(self.markdown_file.convert(f.read()), features='lxml')
 
             text_list = parse.find_all()[2:]
-            # print(text_list)
-            # return
+            
             self.parsed_res[file_num] = {}
             self.parsed_res[file_num]['file_id'] = file_num
             self.parsed_res[file_num]['title'] = file_name[:-3]
@@ -34,13 +29,12 @@ class File_parser():
             cur_level = 1
             pars_level = self.parsed_res[file_num]['sections']
             section_counter = Counter()
-            last_with_selected_level = {}
+            last_with_sel_level = [0] * 6
 
-            last_with_selected_level[0] = self.parsed_res[file_num]['sections']
+            last_with_sel_level[cur_level-1] = self.parsed_res[file_num]['sections']
 
 
             for elem in text_list:
-                # print(text_list)
                 match elem.name:
                     case 'p':
                         pars_level.append({'type':'paragraph', 'text':elem.get_text()})
@@ -49,21 +43,30 @@ class File_parser():
                     case 'li':
                         pars_level[-1]['content'].append(elem.get_text())
                     case 'em':
-                        if 'italicized text' not in pars_level[-1].keys():
-                            pars_level[-1]['italicized text'] = []
-                        pars_level[-1]['italicized text'].append(elem.get_text())
+                        File_parser.append_spec_text(pars_level, 'italicized_text', elem.get_text())
                     case 'strong':
-                        if 'bold text' not in pars_level[-1].keys():
-                            pars_level[-1]['bold text'] = []
-                        pars_level[-1]['bold text'].append(elem.get_text())
+                        File_parser.append_spec_text(pars_level, 'bold_text', elem.get_text())
+                    case 'del':
+                        File_parser.append_spec_text(pars_level, 'strikethrough_text', elem.get_text())
+                    case 'code':
+                        File_parser.append_spec_text(pars_level, 'code_text', elem.get_text())
                     case _:
                         if elem.name[0] == 'h':
                             new_level = int(elem.name[1])
 
-                            if new_level < cur_level:
-                                pars_level = last_with_selected_level[new_level]
-                            elif new_level >= cur_level:
-                                last_with_selected_level[cur_level] = pars_level
+                            #Блок обрабатывает случаи, когда был переход с пропуском уровня(к примеру 1->4), а также удаляет историю уровней с предыдущих блоков     
+                            if new_level <= cur_level:
+                                if last_with_sel_level[new_level-1] != 0:
+                                    pars_level = last_with_sel_level[new_level-1]
+                                else:
+                                    for level in last_with_sel_level[::-1]:
+                                        if level != 0:
+                                            pars_level = level
+                                            break
+                                last_with_sel_level = last_with_sel_level[:new_level] + [0] * (6 - new_level)
+
+                            elif new_level > cur_level:
+                                last_with_sel_level[cur_level] = pars_level
 
                             cur_level = new_level
                             section_counter[cur_level] += 1
@@ -76,16 +79,16 @@ class File_parser():
                             new_section['header'] = elem.get_text()
                             new_section['content'] = []
                             pars_level.append(new_section)
-
+                            
                             pars_level = new_section['content']
-            self.markdown_file.reset()
+
+    @staticmethod
+    def append_spec_text(pars_level, key, text):
+        if key not in pars_level[-1].keys():
+            pars_level[-1][key] = []
+        pars_level[-1][key].append(text)
 
 a = File_parser()
 a.parse_func()
-import pprint
-
-pprint.pprint(json.loads(json.dumps(a.parsed_res), ))
-# with open('lermontov.md', 'w') as f:
-#     f.write(json.dumps(a.parsed_res))
-
-# Добавить обработку кода, зачёркнутых строчек
+with open("data.json", "w") as json_file:
+    json.dump(a.parsed_res, json_file)
